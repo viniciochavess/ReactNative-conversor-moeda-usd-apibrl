a16466ce22a228f9e6a36fa2ac0dad608dadd561
Vinicio Chaves <viniciochavess@gmail.com>
2023-03-28 00:28:59 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,128 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+export  default  function App() {
+
+   const [result, setResult] = useState('');
+   
+   const [value1, setValue1] = useState('');
+   
+   const [value2, setvalue2] = useState('');
+
+   const [cotacao, setCotacao] = useState('');
+   const [cotacao1, setCotacao1] = useState('');
+
+  
+
+   function converter(num:string){
+
+      if(num.includes(',')){
+        
+        return num.replace(',','.')
+      }
+      return num
+
+     
+
+
+   }
+
+   function converterInverte(num:string){
+
+    if(num.includes('.')){
+      
+      return num.replace('.',',')
+    }
+    return num
+
+   
+
+
+ }
+
+   function sum(){
+        let a = (converter(value1))
+        let b = (converter(value2))
+      
+        let resultSum = Number(a) * Number(b);
+        setResult('R$: '+ converterInverte(resultSum.toFixed(1))+'0')
+        
+      
+      
+  
+   }
+   
+
+   useEffect(()=>{
+    if(cotacao == ''){
+      fetch('https://economia.awesomeapi.com.br/last/USD-BRL')
+      .then((response)=> response.json())
+      .then((json)=>{return (setvalue2(String(Number(json.USDBRL.ask))))})
+      .then(()=>{setCotacao(value2)})
+      .catch(()=>{'0'})
+   
+       console.log('teste')
+  
+     }
+   
+    
+
+   },)
+
+
+
+
+   if(cotacao == ''){
+
+    return (
+      <View style={styles.container}>
+       
+       <Text style={styles.title}>Conversor de moeda</Text>
+       
+
+       <Text>Quantidade em dólar</Text>
+       <TextInput style={styles.input} keyboardType='number-pad' onChangeText={setValue1}></TextInput>
+       <Text>Cotação do dólar</Text>
+       <TextInput style={styles.input} keyboardType='number-pad' onChangeText={setvalue2}></TextInput>
+ 
+       <TouchableOpacity style={styles.button} onPress={sum}>
+           <Text style={styles.textButton}>Calcular</Text>
+       </TouchableOpacity>
+ 
+       <Text style={styles.result}>{result}</Text>
+       <Text>{cotacao == ''? "Sem acesso a internet": cotacao }</Text>
+       <StatusBar style="auto" />
+     </View>
+   )
+
+   }
+
+      return (
+     <View style={styles.container}>
+      
+      <Text style={styles.title}>Conversor de moeda</Text>
+     
+      <Text></Text>
+      <Text>Quantidade em dólar</Text>
+      <TextInput style={styles.input} keyboardType='number-pad' onChangeText={setValue1}></TextInput>
+      <Text>Cotação do dólar</Text>
+      <TextInput style={styles.input} keyboardType='number-pad' onChangeText={setvalue2}  defaultValue={Number(cotacao).toFixed(2)}></TextInput>
+
+      <TouchableOpacity style={styles.button} onPress={sum}>
+          <Text style={styles.textButton}>Calcular</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.result}>{result}</Text>
+      <Text>{cotacao == ''? "Sem acesso a internet": "Cotação Atual: "+ Number(cotacao).toFixed(2) }</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
+   
+   
+ 
+
+
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +132,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input:{
+    width:250,
+    height:40,
+    borderRadius:10,
+    backgroundColor:'#ddd',
+    marginTop:20,
+    marginBottom:20,
+    textAlign:'center'
+  },
+  title:{
+    fontSize:22,
+    marginBottom:22,
+    padding:10,
+  
+
+  },
+  button:{
+    backgroundColor:'#ccc',
+    padding:20,
+    borderRadius:8,
+
+  },
+  textButton:{
+    color:'#444'
+
+  },
+
+  result:{
+   
+    marginTop:30,
+    fontSize:22
+  }
 });
